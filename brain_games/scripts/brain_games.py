e99d3490e16562f25f4ecf19aaa6dd1288634eac
Dmirty_Mirazimov <mirazimov@gmail.com>
2023-01-27 00:35:33 +0300

    edit file

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env/python
-from brain_games.cli import welcome_user
-
-
-def main():
-    print('Welcome to the Brain Games!')
-    welcome_user()
-
-
-if __name__ == '__main__':
-    main()
+# #!/usr/bin/env python3
+# from brain_games.cli import welcome_user
+#
+#
+# def main():
+#     print('Welcome to the Brain Games!')
+#
+#     welcome_user()
+#
+#
+# if __name__ == '__main__':
+#     main()
