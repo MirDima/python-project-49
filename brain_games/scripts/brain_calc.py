57f8a54905e48732b2a8030aedf89a6bbab6db2d
Dmitry <mirazimov@gmail.com>
2023-03-02 16:03:21 +0300

    refactor code

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,11 +2,11 @@
 
 
 from brain_games.games import calc
-from brain_games.logic import logic
+from brain_games.logic import run_logic
 
 
 def main():
-    logic(calc)
+    run_logic(calc)
 
 
 if __name__ == '__main__':
