628d5faef0c2c60c456993130f970a60a483d4dc
Dmitry <mirazimov@gmail.com>
2023-03-02 16:04:06 +0300

    refactor code

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,25 +1,25 @@
 from random import randint
 from random import choice
 
-RULES = 'What is the result of the expression?'
+GAME_RULES = 'What is the result of the expression?'
 MIN_NUMBER = 1
 MAX_NUMBER = 50
 
 
 def get_data():
-    num_one = randint(MIN_NUMBER, MAX_NUMBER)
-    operators = choice(['+', '-', '*'])
-    num_two = randint(MIN_NUMBER, MAX_NUMBER)
-    question = f'{num_one} {operators} {num_two}'
-    correct_answer = str(operator_result(num_one, operators, num_two))
+    number1 = randint(MIN_NUMBER, MAX_NUMBER)
+    operator = choice(['+', '-', '*'])
+    number2 = randint(MIN_NUMBER, MAX_NUMBER)
+    question = f'{number1} {operator} {number2}'
+    correct_answer = str(operator_result(number1, operator, number2))
     return question, correct_answer
 
 
-def operator_result(num_one, operators, num_two):
-    if operators == '+':
-        result = num_one + num_two
-    if operators == '-':
-        result = num_one - num_two
-    if operators == '*':
-        result = num_one * num_two
+def operator_result(number1, operator, number2):
+    if operator == '+':
+        result = number1 + number2
+    if operator == '-':
+        result = number1 - number2
+    if operator == '*':
+        result = number1 * number2
     return result
