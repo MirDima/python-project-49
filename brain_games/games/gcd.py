31df14ca336edbf9acf2b7b70a16b7b5753e3968
Dmitry <mirazimov@gmail.com>
2023-03-02 16:04:39 +0300

    refactor code

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,14 +1,14 @@
 import math
 from random import randint
 
-RULES = 'Find the greatest common divisor of given numbers.'
+GAME_RULES = 'Find the greatest common divisor of given numbers.'
 MIN_NUMBER = 2
 MAX_NUMBER = 50
 
 
 def get_data():
-    num_one = randint(MIN_NUMBER, MAX_NUMBER)
-    num_two = randint(MIN_NUMBER, MAX_NUMBER)
-    question = f'{num_one} {num_two}'
-    correct_answer = str(math.gcd(num_one, num_two))
+    number1 = randint(MIN_NUMBER, MAX_NUMBER)
+    number2 = randint(MIN_NUMBER, MAX_NUMBER)
+    question = f'{number1} {number2}'
+    correct_answer = str(math.gcd(number1, number2))
     return question, correct_answer
