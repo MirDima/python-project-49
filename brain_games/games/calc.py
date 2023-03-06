5d25cf731b37cdd42161a6c1701074c8970fc52a
Dmitry <mirazimov@gmail.com>
2023-03-06 21:49:12 +0300

    edit variable name of the game rules

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,7 +1,7 @@
 from random import randint
 from random import choice
 
-GAME_RULES = 'What is the result of the expression?'
+GAME_DESCRIPTION = 'What is the result of the expression?'
 MIN_NUMBER = 1
 MAX_NUMBER = 50
 
